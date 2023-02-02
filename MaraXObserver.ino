2ed66b097c41fc6d2c8225d757ee9cb5243ae353
Simon Ziehme <simon.ziehme@de.bosch.com>
2023-02-02 08:31:28 +0100

    Squashed commit of the following:
    commit 9425ad59b65d91c3c2b12c29c2e8f5cafd0b7ea2
    Author: Simon Ziehme <simon.ziehme@de.bosch.com>
    Date:   Thu Feb 2 08:27:05 2023 +0100
    
        Update MaraXObserver.ino
    
        unnecessary stuff removed

--- MaraXObserver.ino
+++ MaraXObserver.ino
@@ -1,21 +1,10 @@
 /*********************************************************************
 
 *********************************************************************/
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <SoftwareSerial.h>
-
-// Wireless Login
-const char* ssid     = "SZINetzwerk";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "Blue-eyes1405";     // The password of the Wi-Fi network
-
-// initialize webserver
-ESP8266WebServer server(80);
 
 // Serial Port PINS ESP8266
 #define D5              14
@@ -37,59 +26,6 @@ Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 
 // initialize the serial port
 SoftwareSerial mySerial(D5, D6);
 
-// Splash settings
-#define DELAY_SPLASH    75
-
-String sWelcomeStringArray[45] = {
-  "         H",
-  "        Ha",
-  "       Hal",
-  "      Hall",
-  "     Hallo",
-  "    Hallo,",
-  "   Hallo, ",
-  "  Hallo, h",
-  " Hallo, hi",
-  "Hallo, hie",
-  "allo, hier",
-  "llo, hier ",
-  "lo, hier i",
-  "o, hier is",
-  ", hier ist",
-  " hier ist ",
-  "hier ist d",
-  "ier ist de",
-  "er ist dei",
-  "r ist dein",
-  " ist deine",
-  "ist deine ",
-  "st deine L",
-  "t deine Le",
-  " deine Lel",
-  "deine Leli",
-  "eine Lelit",
-  "ine Lelit ",
-  "ne Lelit M",
-  "e Lelit Ma",
-  " Lelit Mar",
-  "Lelit Mara",
-  "elit Mara ",
-  "lit Mara X",
-  "it Mara X!",
-  "t Mara X! ",
-  " Mara X!  ",
-  "Mara X!   ",
-  "ara X!    ",
-  "ra X!     ",
-  "a X!      ",
-  " X!       ",
-  "X!        ",
-  "!         ",
-  "          ",
-};
-
-
-
 // variables
 bool gbInit = true;
 bool gbPumpOn = true;
@@ -99,7 +35,6 @@ int giHx = 32;
 int giBo = 110;
 int giCurrentCounter = 1;
 int giLastCounter = 0;
-
 long lLastMillis = 0;
 
 //Internals
@@ -122,29 +57,23 @@ char rc;
 
 //////////////////////////////////////////////////////////////////////////////////////
 void setup()   {
-//////////////////////////////////////////////////////////////////////////////////////
-
-  
-  WiFi.begin(ssid, password);  
-
+  //////////////////////////////////////////////////////////////////////////////////////
   delay(250); // wait for the OLED to power up
   display.begin(i2c_Address, true); // Address 0x3C default
   display.clearDisplay();
-  
-  //display.setContrast (0); // dim display   
 
   Serial.begin(9600);
   mySerial.begin(9600);
   memset(buffer, 0, BUFFER_SIZE);
-  mySerial.write(0x11);  
+  mySerial.write(0x11);
 
   showMessage("Waiting for the Mara X");
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
 void showMessage(String message)  {
-//////////////////////////////////////////////////////////////////////////////////////
-  display.clearDisplay();    
+  //////////////////////////////////////////////////////////////////////////////////////
+  display.clearDisplay();
   // text display tests
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
@@ -175,11 +104,11 @@ void getMaraData()
   while (mySerial.available())
   {
     serialTimeout = millis();
-    char rcv = mySerial.read();    
+    char rcv = mySerial.read();
     if (rcv != '\n')
       buffer[iIndex++] = rcv;
     else {
-      iIndex = 0;      
+      iIndex = 0;
       char* ptr = strtok(buffer, ",");
       int idx = 0;
       while (ptr != NULL)
@@ -193,166 +122,42 @@ void getMaraData()
 
   if (millis() - serialTimeout > 6000)
   {
-    
+
     serialTimeout = millis();
-    mySerial.write(0x11);    
+    mySerial.write(0x11);
   }
-}
-
-//////////////////////////////////////////////////////////////////////////////////////
-void startNetwork() {
-//////////////////////////////////////////////////////////////////////////////////////
-  display.clearDisplay();    
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-  int iConnectTry = 0;
-  display.clearDisplay();    
-    display.setCursor(0, 0);
-    display.println("Verbindungsaufbau");
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    display.print(".");
-    delay(1000);
-    display.display();
-    iConnectTry++;
-    if (iConnectTry > 10)
-       break;
-  }
-  if (iConnectTry > 10) {
-    display.println("");
-    display.println("");
-    display.println("Verbindung");
-    display.println("nicht moeglich!");  
-    display.display();
-    delay(1000);
-    return;
-  }
-
-  display.clearDisplay();    
-  display.println("");
-  display.println("Verbindung");
-  display.println("");
-  display.println("aufgebaut!");  
-
-  server.on("/", indexHTML);
-  server.on("/TempWeb", TempMessung);
-  
-  server.begin();
-  
-  display.display();
-  delay(1000);
-  display.clearDisplay();    
-  display.setCursor(0, 0);
-  display.println("");
-  display.println("IP Addresse");
-  display.println("");
-  display.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-  display.display();
-  delay(3000); 
-  
-}
-
-//////////////////////////////////////////////////////////////////////////////////////
-void indexHTML() {    
-//////////////////////////////////////////////////////////////////////////////////////
-  server.send(200, "text/html", 
-  "<!DOCTYPE html>\
-  <html>\
-  <head>\
-  <title>Temperaturanzeige</title>\
-  <meta http-equiv='content-type' content='text/html'; charset='utf-8'>\
-  <style>\
-  body { background-color: #585858; font-size: 50px; font-family: Arial, Helvetica, Sans-Serif; color: #F2F2F2; margin-left: 40px; }\
-  h1 { color: #2ECCFA; margin-top: 50px; margin-bottom: 0px; }\
-  h2 { font-size: 20px; margin-top: 0px; margin-bottom: 50px; }\
-  #temp { width: 230px; height: 80px; border: 5px solid #F2F2F2; text-align:center; padding: 1px; color: #9AFE2E; background-color: #000000; font-size: 60px; }\
-  </style>\
-  </head>\
-   <body>\
-  <h1>Lelit Mara X</h1>\
-    <table>\
-  <tr><td>HX Temperatur:&nbsp;</td>\
-  <td id='temp'><span id='TempWert'>-</span>°C</td></tr>\
-  <tr><td>Boiler Temperatur:&nbsp;</td>\
-  <td id='temp'><span id='BoTempValue'>-</span>°C</td></tr>\
-  <tr><td>Prioritaet:&nbsp;</td>\
-  <td id='temp'><span id='PrioValue'>-</span></td></tr>\
-  <tr><td>Heizung:&nbsp;</td>\
-  <td id='temp'><span id='HeaterValue'>-</span></td></tr>\
-  <tr><td>Pumpe:&nbsp;</td>\
-  <td id='temp'><span id='PumpValue'>-</span></td></tr>\
-  </table>\
-  <script>\
-  setInterval(function() {\
-    getData();\
-  }, 1000);\
-  function getData() {\
-    var xhttp = new XMLHttpRequest();\
-    xhttp.onreadystatechange = function() {\
-      if (this.readyState == 4 && this.status == 200) {\
-        document.getElementById('TempWert').innerHTML = this.responseText;\
-      }\
-    };\
-    xhttp.open('GET', 'TempWeb', true);\
-    xhttp.send();\
-  }\
-  </script>\
-  </body>\
-  </html>\
-  ");
-}
-
-//////////////////////////////////////////////////////////////////////////////////////
-void showSplash() {
-//////////////////////////////////////////////////////////////////////////////////////
-  
-  // Clear the buffer.
-  display.clearDisplay();
-    
-  // text display tests
-  display.setTextSize(2);
-  display.setTextColor(SH110X_WHITE);
-  
-  for (int i = 1; i<46;i++) {
-   display.setCursor(0, 30);
-   display.print(sWelcomeStringArray[i]);    
-   display.display();
-   delay(DELAY_SPLASH);
-   display.clearDisplay();
-  }  
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
 // show the main screen with the temperature information
 void showMain(bool bCoffeeMode, bool bHeaterOn, String sHXTemp, String sBoTemp)   {
-//////////////////////////////////////////////////////////////////////////////////////  
-  display.clearDisplay();  
+  //////////////////////////////////////////////////////////////////////////////////////
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setCursor(0, 0);
   if (bCoffeeMode == true)
-     display.print("Kaffee-Mode   |||:");
+    display.print("Kaffee-Mode   |||:");
   else
-     display.print("Dampf-Mode    |||:");
+    display.print("Dampf-Mode    |||:");
 
   if (bHeaterOn == true)
-     display.println("An");
+    display.println("An");
   else
-     display.println("Aus");
+    display.println("Aus");
 
   display.println("---------------------");
   //display.println("");
   display.setTextSize(2);
-  
+
   display.print("HX: ");
   display.print(sHXTemp);
   display.print(" ");
   display.print((char)247);
   display.println("C");
-  
+
   display.print("Bo: ");
-      
+
   display.print(sBoTemp);
   display.print(" ");
   display.print((char)247);
@@ -363,65 +168,48 @@ void showMain(bool bCoffeeMode, bool bHeaterOn, String sHXTemp, String sBoTemp) 
   display.print("letzter Bezug: ");
   display.print(giLastCounter);
   display.println(" s");
-  display.display();   
-  
+  display.display();
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
-// show the counter screen; if pump is on this screen is active 
+// show the counter screen; if pump is on this screen is active
 void showCounter(bool bCoffeeMode, bool bHeaterOn, String sCounter)   {
-//////////////////////////////////////////////////////////////////////////////////////
-  display.clearDisplay();  
+  //////////////////////////////////////////////////////////////////////////////////////
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setCursor(0, 0);
   if (bCoffeeMode == true)
-     display.print("Kaffee-Mode   |||:");
+    display.print("Kaffee-Mode   |||:");
   else
-     display.print("Dampf-Mode    |||:");
+    display.print("Dampf-Mode    |||:");
 
   if (bHeaterOn == true)
-     display.println("An");
+    display.println("An");
   else
-     display.println("Aus");
+    display.println("Aus");
 
   display.println("---------------------");
   display.println("");
   display.setTextSize(3);
-  
+
   display.print("   ");
   if (sCounter.toInt() < 10)
-     display.print(" ");
+    display.print(" ");
   display.println(sCounter);
-  
+
   display.setTextSize(1);
   //display.println("");
   display.println("---------------------");
   display.print("letzter Bezug: ");
   display.print(giLastCounter);
-  display.println(" s");  
-  display.display();   
-}
-
-//////////////////////////////////////////////////////////////////////////////////////
-void TempMessung() {
-//////////////////////////////////////////////////////////////////////////////////////  
-  String TempWert = String(giHx); 
-  server.send(200, "text/plane", TempWert);
+  display.println(" s");
+  display.display();
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-//////////////////////////////////////////////////////////////////////////////////////
-
-  /*if (gbInit) {
-    //startNetwork();
-    showSplash();    
-    gbInit = false;
-  }
-*/
-  //server.handleClient();
-
+  //////////////////////////////////////////////////////////////////////////////////////
   getMaraData();
 
   if (lastMaraData != NULL) {
@@ -434,27 +222,27 @@ void loop() {
       gbHeaterOn = true;
     else
       gbHeaterOn = false;
-  
+
     if (lastMaraData[6].toInt() > 0)
       gbPumpOn = true;
     else
       gbPumpOn = false;
-    
+
     if (gbPumpOn)  {
-      
+
       showCounter(gbCoffeeMode, gbHeaterOn, String(giCurrentCounter));
       if ( millis() - lastMillis >= 1000) {
         lastMillis = millis();
-        ++giCurrentCounter;        
-      if (giCurrentCounter > 99)
-        giCurrentCounter = 0;
-      }            
+        ++giCurrentCounter;
+        if (giCurrentCounter > 99)
+          giCurrentCounter = 0;
+      }
     }
     else {
       if (giCurrentCounter > 0)
         giLastCounter = giCurrentCounter;
       giCurrentCounter = 0;
-      showMain(gbCoffeeMode, gbHeaterOn, lastMaraData[3], lastMaraData[1]);  
+      showMain(gbCoffeeMode, gbHeaterOn, lastMaraData[3], lastMaraData[1]);
     }
-  }  
+  }
 }
